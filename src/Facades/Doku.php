66820c6d769a010c079bc227f4c3cfab67cb5c76
Hade Mopilie <hademopilie@gmail.com>
2023-04-19 07:32:10 +0800

    feat: add method checkout

--- src/Facades/Doku.php
+++ src/Facades/Doku.php
@@ -4,11 +4,13 @@ namespace Jawak\DokuLaravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Jawak\DokuLaravel\Service\CheckOut CheckOut()
  * @method static \Jawak\DokuLaravel\Service\VirtualAccount VA()
  * @method static \Jawak\DokuLaravel\Service\CreditCard CC()
  * @method static \Jawak\DokuLaravel\Service\EMoney EMoney()
  * @method static \Jawak\DokuLaravel\Service\Gerai Gerai()
  */
+
 class Doku extends Facade
 {
     protected static function getFacadeAccessor()
