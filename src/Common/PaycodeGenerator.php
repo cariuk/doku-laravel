d827afefcb272a8d3eccd86dcff07de558203d73
Hade Mopilie <hademopilie@gmail.com>
2023-05-17 20:41:06 +0800

    feat: get status payment

--- src/Common/PaycodeGenerator.php
+++ src/Common/PaycodeGenerator.php
@@ -10,6 +10,38 @@ use Jawak\DokuLaravel\Doku;
 
 class PaycodeGenerator
 {
+    public static function get($params)
+    {
+        $config = Doku::getConfig();
+        $header = [];
+        $requestId = rand(1, 100000);
+        $dateTime = gmdate("Y-m-d H:i:s");
+        $dateTime = date(DATE_ISO8601, strtotime($dateTime));
+        $dateTimeFinal = substr($dateTime, 0, 19) . "Z";
+
+        $getUrl = Config::getBaseUrl($config['environment']);
+
+        $targetPath = $params['targetPath'];
+        $url = $getUrl . $targetPath;
+        unset($params['targetPath']);
+        $data = $params;
+
+        $header['Client-Id'] = $config['client_id'];
+        $header['Request-Id'] = $requestId;
+        $header['Request-Timestamp'] = $dateTimeFinal;
+        $signature = Utils::generateSignature($header, $targetPath, json_encode($data), $config['shared_key']);
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Signature' => $signature,
+            'Request-Id' =>  $requestId,
+            'Client-Id' =>  $config['client_id'],
+            'Request-Timestamp' => $dateTimeFinal,
+            'Request-Target' => $targetPath,
+        ];
+
+        $resp = Http::timeout(120)->connectTimeout(120)->withHeaders($headers)->get($url, $data);
+        return $resp->collect();
+    }
 
     public static function post($params)
     {
--- src/Service/CheckOut.php
+++ src/Service/CheckOut.php
@@ -11,4 +11,10 @@ class CheckOut
         $params['targetPath'] = '/checkout/v1/payment';
         return PaycodeGenerator::post($params);
     }
+
+    public function getStatus($params)
+    {
+        $params['targetPath'] = '/checkout/v1/status/'.$params['order']['invoice_number'];
+        return PaycodeGenerator::get($params);
+    }
 }
